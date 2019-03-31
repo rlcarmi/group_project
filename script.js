5751b906f31c0db525b10aa1ca6ee38548b33a9f
mysticmathlete <47639900+mysticmathlete@users.noreply.github.com>
2019-03-31 17:05:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $('#upload').click(function(){
+
+    var fd = new FormData();
+    var files = $('#file')[0].files[0];
+    fd.append('file',files);
+
+    // AJAX request
+    $.ajax({
+      url: 'upload.php',
+      type: 'post',
+      data: fd,
+      contentType: false,
+      processData: false,
+      success: function(response){
+        if(response != 0){
+          // Show image preview
+          $('#preview').append("<img src='"+response+"' width='100' height='100' style='display: inline-block;'>");
+        }else{
+          alert('file not uploaded');
+        }
+      }
+    });
+  });
+});
