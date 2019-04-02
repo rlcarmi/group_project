333ec588e9a50254e0b339ccae673acd7af4fd9c
rlcarmi <47615141+rlcarmi@users.noreply.github.com>
2019-04-02 12:11:18 +0100

    Updated the php form

--- mail.php
+++ mail.php
@@ -3,8 +3,8 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
 $recipient = "rlc35@sussex.ac.uk";
-$subject = "Snack-Snap Email";
+$subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank you! Your email has been sent & we will get back to you as soon as we can!";
+echo "Thank you for getting in touch! We will get back to you as soon as we can :)";
 ?>
